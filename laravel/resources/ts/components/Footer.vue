f66e544deb6cc1942034936ff7e8bad904ea5952
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-09-16 00:59:52 +0900

    add auth user interface

---
+++ laravel/resources/ts/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <button class="button button--link">Logout</button>
+    <RouterLink class="button button--link" to="/login">
+      Login / Register
+    </RouterLink>
+  </footer>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Footer extends Vue {
+    }
+</script>
+
+<style scoped>
+
+</style>
