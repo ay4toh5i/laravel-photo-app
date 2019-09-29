aa2dcb1da9f70398537545b5b8b2f075d63a229f
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-09-29 16:18:57 +0900

    return user data if user is logged-in

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/user', function () {
+    return Auth::user();
+})->name('user');
+
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 
 Route::post('/login', 'Auth\LoginController@login')->name('login');
