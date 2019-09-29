989aa84b6052aaf28cb1790d5d59356e3be88936
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-09-29 16:16:49 +0900

    keep login state on frontend

--- laravel/resources/ts/store/auth.ts
+++ laravel/resources/ts/store/auth.ts
@@ -1,16 +1,21 @@
 import window from'../window'
 
 interface User {
+    name: string
 }
 
 interface State {
     user: User | null
 }
+
 const state: State = {
     user: null
 }
 
-const getters = {}
+const getters = {
+    check: (state: State) => { return !! state.user },
+    username: (state: State) => { return state.user ? state.user.name : '' }
+}
 
 const mutations = {
     setUser (state: State, user: User | null) {
@@ -32,7 +37,14 @@ const actions = {
     async logout (context: any) {
         const response = await window.axios.post('/api/logout')
         context.commit('setUser', null)
+    },
+
+    async currentUser (context: any) {
+        const response = await window.axios.get('/api/user')
+        const user = response.data || null
+        context.commit('setUser', user)
     }
+
 }
 
 export default {
